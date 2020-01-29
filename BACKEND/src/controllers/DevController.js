f7a5b05d93f2dad34031f7a973426795423da8dc
esoft11 - Daniel Barreto <dandpb@gmail.com>
2020-01-29 06:30:47 -0300

    add websocket

--- BACKEND/src/controllers/DevController.js
+++ BACKEND/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 //index, show, store, update, destroy
 
@@ -36,6 +37,14 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      // filtrar as conexões que estão há no máximo 10km de distância
+      // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+      const sendSocketMessageTo = findConnections(
+        {latitude, longitude},
+        parseStringAsArray(techs)
+      )
+      sendMessage(sendSocketMessageTo, 'new-dev', dev);
     }
 
     return response.json(dev);
